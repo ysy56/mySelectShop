690d661a5fcbb602b4488f95ac3bba6eacaf43fa
moony <78634780+ysy56@users.noreply.github.com>
2024-05-23 18:57:17 +0900

    [2-15] 관심상품 희망 최저가 업데이트 API

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -3,6 +3,6 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
