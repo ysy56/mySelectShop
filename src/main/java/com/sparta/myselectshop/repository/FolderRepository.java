12e99332dfea1574e942fa592240139438919283
moony <78634780+ysy56@users.noreply.github.com>
2024-05-24 12:43:55 +0900

    [2-21] 폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -1,8 +1,16 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+    // select * from folder where user_id = 1 and name in ('1', '2', '3');
+
+    List<Folder> findAllByUser(User user);
+
 }
